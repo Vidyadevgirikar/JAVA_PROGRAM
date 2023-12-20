8f9ceda46b30b6c19cd6fc51b982ddd4028cf696
Vidya Devgirikar <vidyadevgirikar05@gmail.com>
2023-12-20 17:01:02 +0530

    My First Java Program

---
+++ MyProgram.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class MyProgram
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter two number");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("enter the string");
+char c= Scanner.next().charAt(0);
+System.out.println(c);
+System.out.println(a+b);
+}
+}
